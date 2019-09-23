75e6d1b4af96eb6e8ca6a3853fd393e8df860e57
ingvarChuniak <i.chunyak@gmail.com>
2019-09-24 00:23:01 +0300

    default

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,9 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    thetaTemp =  theta(1) - alpha * (1/m) * sum( ((X * theta) - y) .* X(:, 1));
+    thetaTemp2 = theta(2) - alpha * (1/m) * sum( ((X * theta) - y) .* X(:, 2));
 
-
-
+    theta = [thetaTemp; thetaTemp2]
 
 
 
